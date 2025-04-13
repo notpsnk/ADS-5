e90f33fc87f2d73a25e66849e138045f0762e4cc
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-04-13 17:37:11 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,36 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int maxSize>
 class TStack {
-  // добавьте код стека
+private:
+  T data[maxSize];
+  int topIndex;
+public:
+  TStack() : topIndex(-1) {}
+  void push(const T& value) {
+    if (isFull()) {
+      throw std::overflow_error("Stack overflow");
+    }
+    data[++topIndex] = value;
+  }
+  void pop() {
+    if (isEmpty()) {
+      throw std::underflow_error("Stack underflow");
+    }
+    --topIndex;
+  }
+  T top() const {
+    if (isEmpty()) {
+      throw std::underflow_error("Stack is empty");
+    }
+    return data[topIndex];
+  }
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
+  bool isFull() const {
+    return topIndex == maxSize - 1;
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
