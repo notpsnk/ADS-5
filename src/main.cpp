cd1057fdf47275ff00f83ce4b1539f832156a866
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-04-21 10:25:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
+
+std::string infx2pstfx(const std::string& inf);
+int eval(const std::string& post);
 
 int main() {
-    return 0;
+  std::string inputExpression;
+  std::string postfixForm;
+
+  if (std::getline(std::cin, inputExpression)) {
+    postfixForm = infx2pstfx(inputExpression);
+    std::cout << postfixForm << std::endl;
+
+    int result = eval(postfixForm);
+    std::cout << result << std::endl;
+  }
+
+  return 0;
 }
