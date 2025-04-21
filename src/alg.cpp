e1829439c95f9e8c7e7392a78daf24537179d6af
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-04-22 00:00:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,83 +4,86 @@
 #include <cctype>
 #include "tstack.h"
 
-int precedence(char symbol) {
-  switch (symbol) {
-    case '+':
-    case '-': return 1;
-    case '*':
-    case '/': return 2;
-    default: return 0;
-  }
+int precedence(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;
 }
 
 std::string infx2pstfx(const std::string& inf) {
-  TStack<char, 100> ops;
-  std::string res;
+  std::string output;
+  TStack<char, 100> stack;
+  size_t i = 0;
 
-  for (std::size_t pos = 0; pos < inf.size(); ++pos) {
-    char current = inf[pos];
+  while (i < inf.length()) {
+    char ch = inf[i];
 
-    if (std::isdigit(current)) {
-      std::string val;
-      while (pos < inf.size() && std::isdigit(inf[pos])) {
-        val.push_back(inf[pos]);
-        ++pos;
+    if (isdigit(ch)) {
+      std::string number;
+      while (i < inf.length() && isdigit(inf[i])) {
+        number += inf[i];
+        ++i;
       }
-      res += val + " ";
-      --pos;
-    } else if (current == '(') {
-      ops.push(current);
-    } else if (current == ')') {
-      while (!ops.isEmpty() && ops.top() != '(') {
-        res += ops.top();
-        res += ' ';
-        ops.pop();
-      }
-      if (!ops.isEmpty()) ops.pop();
-    } else if (current == '+' || current == '-' || current == '*' || current == '/') {
-      while (!ops.isEmpty() && precedence(ops.top()) >= precedence(current)) {
-        res += ops.top();
-        res += ' ';
-        ops.pop();
-      }
-      ops.push(current);
+      output += number + " ";
+      continue;
     }
+
+    if (ch == '(') {
+      stack.add(ch);
+    } else if (ch == ')') {
+      while (!stack.empty() && stack.peek() != '(') {
+        output += stack.peek();
+        output += " ";
+        stack.remove();
+      }
+      if (!stack.empty()) stack.remove();
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      while (!stack.empty() && precedence(stack.peek()) >= precedence(ch)) {
+        output += stack.peek();
+        output += " ";
+        stack.remove();
+      }
+      stack.add(ch);
+    }
+
+    ++i;
   }
 
-  while (!ops.isEmpty()) {
-    res += ops.top();
-    res += ' ';
-    ops.pop();
+  while (!stack.empty()) {
+    output += stack.peek();
+    output += " ";
+    stack.remove();
   }
 
-  if (!res.empty() && res.back() == ' ')
-    res.pop_back();
+  if (!output.empty() && output.back() == ' ')
+    output.pop_back();
 
-  return res;
+  return output;
 }
 
 int eval(const std::string& post) {
-  TStack<int, 100> stk;
-  std::istringstream parser(post);
-  std::string item;
+  TStack<int, 100> stack;
+  std::istringstream in(post);
+  std::string token;
 
-  while (parser >> item) {
-    if (std::isdigit(item[0]) || (item[0] == '-' && item.length() > 1)) {
-      stk.push(std::stoi(item));
+  while (in >> token) {
+    if (isdigit(token[0]) || (token[0] == '-' && token.length() > 1)) {
+      stack.add(std::stoi(token));
     } else {
-      int rhs = stk.top(); stk.pop();
-      int lhs = stk.top(); stk.pop();
-      int out = 0;
+      int b = stack.peek(); stack.remove();
+      int a = stack.peek(); stack.remove();
+      int result = 0;
 
-      if (item[0] == '+') out = lhs + rhs;
-      else if (item[0] == '-') out = lhs - rhs;
-      else if (item[0] == '*') out = lhs * rhs;
-      else if (item[0] == '/') out = lhs / rhs;
+      switch (token[0]) {
+        case '+': result = a + b; break;
+        case '-': result = a - b; break;
+        case '*': result = a * b; break;
+        case '/': result = a / b; break;
+      }
 
-      stk.push(out);
+      stack.add(result);
     }
   }
 
-  return stk.top();
+  return stack.peek();
 }
